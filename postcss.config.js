d3a8cb40269077d30c15b12d73f9b9486f4a9d88
loweffort-alt <farfan_alexander@outlook.com>
2023-09-18 04:32:57 -0500

    Updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    plugins: [
-        require("tailwindcss/nesting"),
-        require("tailwindcss"),
-        require("autoprefixer"),
-        ...(process.env.NODE_ENV === "production"
-            ? [
-                require("cssnano")({
-                    preset: "default",
-                }),
-            ]
-            : []),
-    ],
+  plugins: [
+    require("tailwindcss/nesting"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          require("cssnano")({
+            preset: "default",
+          }),
+        ]
+      : []),
+  ],
 };
